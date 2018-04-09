abbcec76415c0df67ec51b2da0f2420a67418ae8
Tjerk Valentijn <tjerk.valentijn@inology.nl>
2018-04-09 09:15:43 +0200

    got gedcom model loading

--- src/familytree.js
+++ src/familytree.js
@@ -1,25 +1,20 @@
 /**
- * Tree modeled on Gedcom standard, instead of the pure biological family (no marriage and divorce)
+ * Tree modeled on the pure biological family (no marriage and divorce) instead of the gedcom christian marriage/husband/wife concept
  * @type {{people: *[], families: *[]}}
  */
 const tree = {
   people: [
-    {id: "123abc", lastname: "Valentijn", firstnames: ["Tjerk"], birthdate: "1976-05-18", deathdate: ""},
-    {id: "456dfa", lastname: "Valentijn", firstnames: ["Leo", "Gerard"], birthdate: "1952-03-28", deathdate: ""},
+    {id: "123abc", fatherId: "456dfa", motherId: "789wsh", lastname: "Valentijn", firstnames: ["Tjerk"], birthdate: "1976-05-18", deathdate: ""},
+    {id: "456dfa", fatherId: "abc123", motherId: "def321", lastname: "Valentijn", firstnames: ["Leo", "Gerard"], birthdate: "1952-03-28", deathdate: ""},
     {id: "789wsh", lastname: "Valentijn", firstnames: ["Marianne", "Sophia"], birthdate: "1951-07-18", deathdate: ""},
-    {id: "652hsd", lastname: "Valentijn", firstnames: ["Mirthe"], birthdate: "1979-03-14", deathdate: ""},
+    {id: "652hsd", fatherId: "456dfa", motherId: "789wsh", lastname: "Valentijn", firstnames: ["Mirthe"], birthdate: "1979-03-14", deathdate: ""},
 
     {id: "851sdh", lastname: "Volkers", firstnames: ["Tineke"], birthdate: "1983-12-29", deathdate: ""},
-    {id: "4815klo", lastname: "Valentijn", firstnames: ["Nora", "Luz"], birthdate: "2008-04-02", deathdate: ""},
-    {id: "321igf", lastname: "Valentijn", firstnames: ["Abigail", "Sophia"], birthdate: "2010-03-07", deathdate: ""},
+    {id: "4815klo", fatherId: "123abc", motherId: "851sdh", lastname: "Valentijn", firstnames: ["Nora", "Luz"], birthdate: "2008-04-02", deathdate: ""},
+    {id: "321igf", fatherId: "123abc", motherId: "851sdh", lastname: "Valentijn", firstnames: ["Abigail", "Sophia"], birthdate: "2010-03-07", deathdate: ""},
 
     {id: "abc123", lastname: "Valentijn", firstnames: ["Cornelis"], birthdate: "1916-04-26", deathdate: "1965-06-01"},
     {id: "def321", lastname: "Visser", firstnames: ["Marijtje"], birthdate: "1918-03-05", deathdate: "1999-06-21"},
-  ],
-  families: [
-    {id: "g98475kgf", fatherId: "123abc", motherId: "851sdh", childrenIds: ["4815klo", "321igf"]},
-    {id: "kgfh98995", fatherId: "456dfa", motherId: "789wsh", childrenIds: ["123abc", "652hsd"]},
-    {id: "f54906865", fatherId: "abc123", motherId: "def321", childrenIds: ["456dfa"]},
   ]
 };
 
--- src/familytreeManager.js
+++ src/familytreeManager.js
@@ -1,28 +1,22 @@
+import uuidv1 from "uuid/v1";
 
 export const findPersonById = (personId, people) => people.find(person => person.id === personId);
 
+export const makePerson = props => { return { id: uuidv1(), father: {id: null} } };
+
 export const enrichTree = tree => {
   const newTree = {...tree};
-  newTree.families.map(fam => {
-    fam.father = findPersonById(fam.fatherId, tree.people);
-    fam.mother = findPersonById(fam.motherId, tree.people);
-    fam.children = fam.childrenIds.map(cid => findPersonById(cid, tree.people));
-    return fam;
-  });
-
+  // TODO use Maybe type
   newTree.people.map(person => {
-    const fam = getFamilyByChild(person.id, newTree.families);
-    if (fam) {
-      person.father = findPersonById(fam.fatherId, tree.people);
-      person.mother = findPersonById(fam.motherId, tree.people);
-      // TODO set siblings in person?
-    }
-    return fam;
+    person.father = (person.fatherId) ? findPersonById(person.fatherId, tree.people) : makePerson();
+    person.mother = (person.motherId) ? findPersonById(person.motherId, tree.people) : makePerson();
+    // TODO set siblings in person?
+    return person;
   });
   return newTree;
 };
 
-export const getFamilyByChild = (childId, families) => families.find(family => family.childrenIds.includes(childId));
+// export const getFamilyByChild = (childId, families) => families.find(family => family.childrenIds.includes(childId));
 
 export const isParent = (p1, p2) => (p1.father === undefined) ? false : (p1.father.id === p2.id || p1.mother.id === p2.id);
 
